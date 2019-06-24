8ca36d67a5af6694c5469bc42403f2bd84a379d7
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-24 21:12:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,7 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   coupon_cart = apply_coupons(consolidated_cart, coupons)
   clearance_and_coupon = apply_clearance(coupon_cart)
-  total = clearance_and_coupon.reduce(0) {|total, (key, value)| total += (value[:price] * value[:count]}
+  total = clearance_and_coupon.reduce(0) {|total, (key, value)| total += (value[:price] * value[:count])}
   binding.pry
   if total < 100
     return total
