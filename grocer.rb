a2d609aebaf57f57328023f3a5e2d0d7d00df1a3
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-24 18:31:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,10 @@ result
 end
 
 def apply_coupons(cart, coupons)
+  puts coupons[0]
 if coupons[0]
+  coupon = coupons[0]
+end
  if cart.has_key?(coupon[:item])
     item = coupon[:item] 
     with_coupon = "#{item} W/COUPON"
